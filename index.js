24c31c30e081c0c8c1a980b9aef20d3b1d2f13f2
Naominm <naomimbugua536@gmail.com>
2025-04-04 16:29:24 +0300

    Add updating a movie

--- index.js
+++ index.js
@@ -6,12 +6,52 @@ const app = express();
 app.use(express.json());
 const client = new PrismaClient();
 
-app.get("/movies", (req, res) => {
-  res.send("Getting all movies");
+app.get("/movies", async (_req, res) => {
+ try {
+    const movies= await client.movieItem.findMany()
+    res.status(200).json({
+        status:"Success",
+        message:"Successfully fetched movies",
+        data:movies
+    })
+ } catch (e) {
+   res.status(500).json({
+    status:"Error",
+    message:"Something went wrong. Please try again"
+   }) 
+ }
 });
 
-app.get("/movies/:movieId", (req, res) => {
-  res.send(`Getting specific movie`);
+app.get("/movies/:movieId", async (req, res) => {
+    const movieId=req.params;
+try {
+      const Movie= await client.movieItem.findFirst({
+        where:{
+            id:movieId
+        }
+      })
+      if(!Movie){
+        res.status(404).json({
+            status:"Error",
+            message:"Task not found"
+        })
+      }
+
+        res.status(200).json({
+            status:"Success",
+            message:"Successfully fetched a movie",
+            data:Movie
+    
+          })
+      
+} catch (e) {
+    res.status(500).json({
+        status:"Error",
+        message:"something went wrong, Please try again"
+
+    })
+    
+}
 });
 
 app.post("/movies", [validateMovie], async (req, res) => {
@@ -33,12 +73,34 @@ app.post("/movies", [validateMovie], async (req, res) => {
   }
 });
 
-app.patch("/movies/:movieId", (req, res) => {
-  res.send(`Updating a specific movie`);
+app.patch("/movies/:movieId", async (req, res) => {
+const {movieTitle,movieDescription,isWatched}=req.body;
+try {
+    const updateMovie=await client.movieItem.update({
+        where:{
+            id: req.params.movieId
+        },
+        data:{
+          movieTitle:movieTitle&&movieTitle,
+          movieDescription:movieDescription&&movieDescription,
+          isWatched:isWatched&&isWatched
+        }
+    })
+    res.status(200).json({
+      status:"Success",
+      message:"Task updated successfully",
+      data:updateMovie
+    })
+} catch (e) {
+   res.status(500).json({
+    status:"Error",
+    message:"Something Went Wrong"
+   })  
+}
 });
 
 app.delete("/movies/:movieId", (req, res) => {
-  res.send(`Deleting a specific Movie`);
+
 });
 
 let port = process.env.PORT || 3000;
